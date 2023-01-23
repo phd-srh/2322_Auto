3f62b943b5ed336e14cde4c95af3f75c5535d2d0
Dr. Dillinger <peter.dillinger@srh.de>
2023-01-23 11:36:31 +0100

    Projekt vollständig (laut Aufgabenstellung)

--- src/Auto.java
+++ src/Auto.java
@@ -1,2 +1,13 @@
 public class Auto {
+    // Attribute
+    private int achsenanzahl;
+
+    public Auto(int achsenanzahl) {
+        this.achsenanzahl = achsenanzahl;
+    }
+
+    // Methode(n)
+    public void fahren() {
+        System.out.println("Auto fährt mit " + achsenanzahl + " Achsen");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        Auto auto1 = new Auto(2);
+        Auto auto2 = new Auto(4);
 
+        auto1.fahren();
+        auto2.fahren();
     }
 }
